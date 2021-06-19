df8221edc61d6020110db63d1c210db6672878fd
Mickey Kim <mickeykkim@gmail.com>
2021-06-19 15:10:33 +0100

    Refactor hard coded strings into functions

--- inky_pi/inky_pi.py
+++ inky_pi/inky_pi.py
@@ -1,4 +1,5 @@
-"""Main module."""
+# -*- coding: utf-8 -*-
+"""Fetches Train and Weather data and displays on a Raspberry Pi w/InkyWHAT."""
 from time import strftime
 
 import requests
@@ -6,79 +7,159 @@ from font_hanken_grotesk import HankenGroteskBold
 from inky import InkyWHAT
 from PIL import Image, ImageDraw, ImageFont
 
-# Train Constants
+# Train constants - Maze Hill to London Bridge, 3 trains
 STATION_FROM = 'MZH'
 STATION_TO = 'LBG'
 ARRIVAL_NUM = 3
 
-# Weather Constants
+# Weather constants - London, GB
 LAT = 51.5085
 LON = -0.1257
 EXCL = 'minutely,hourly'
-W_API_KEY = ""
-KELVIN_CONV = 273.15
+W_API_KEY = ""  # Keep secret
 
+# Weather conversion/formatting constants
+KELVIN_CONV_C = -273.15
+DEG_C = u"\N{DEGREE SIGN}" + "C"
+DEG_F = u"\N{DEGREE SIGN}" + "F"
+
+# Inky display variables
 inky_display = InkyWHAT('black')
-inky_display.set_border(inky_display.BLACK)
+IBLACK = inky_display.BLACK
+inky_display.set_border(IBLACK)
 
+# Image drawing variables
 img = Image.new('P', (inky_display.WIDTH, inky_display.HEIGHT))
 draw = ImageDraw.Draw(img)
 
-font_s = ImageFont.truetype(HankenGroteskBold, 25)
-font_l = ImageFont.truetype(HankenGroteskBold, 35)
+# Fonts
+FONT_S = ImageFont.truetype(HankenGroteskBold, 25)
+FONT_L = ImageFont.truetype(HankenGroteskBold, 35)
 
+# Train, Weather data
 response_train = requests.get(
     'https://huxley2.azurewebsites.net/arrivals/'
     f'{STATION_FROM}/from/{STATION_TO}/{ARRIVAL_NUM}')
-
+data_t = response_train.json()
 payload_weather = {'lat': LAT, 'lon': LON, 'exclude': EXCL, 'appid': W_API_KEY}
-
 response_weather = requests.get(
     'https://api.openweathermap.org/data/2.5/onecall?', params=payload_weather)
-
-data_t = response_train.json()
 data_w = response_weather.json()
 
-str_date_time = strftime('%a %d %b %Y - %H:%M')
 
-try:
-    str_train1 = '1) ' + \
-        data_t['trainServices'][0]['origin'][0]['locationName'] + \
-        ' - ' + data_t['trainServices'][0]['sta']
-    str_train2 = '2) ' + \
-        data_t['trainServices'][1]['origin'][0]['locationName'] + \
-        ' - ' + data_t['trainServices'][1]['sta']
-    str_train3 = '3) ' + \
-        data_t['trainServices'][2]['origin'][0]['locationName'] + \
-        ' - ' + data_t['trainServices'][2]['sta']
-except KeyError:
-    str_train1 = "Error"
-    str_train2 = "Error"
-    str_train3 = "Error"
+def gen_train_string(data: dict, n: int) -> str:
+    """Generate train string
 
-try:
-    str_w_curr = ("{:.1f}".format(data_w['current']['temp'] - KELVIN_CONV) +
-                  u"\N{DEGREE SIGN}" + "C - " +
-                  data_w['current']['weather'][0]['main'])
-    str_w_today = (
-        "Today: " +
-        "{:.1f}".format(data_w['daily'][0]['temp']['min'] - KELVIN_CONV) +
-        u"\N{DEGREE SIGN}" + "C - " +
-        "{:.1f}".format(data_w['daily'][0]['temp']['max'] - KELVIN_CONV) +
-        u"\N{DEGREE SIGN}" + "C")
-    str_w_desc = (data_w['daily'][0]['weather'][0]['description'])
-except KeyError:
-    str_w_curr = "Error"
-    str_w_today = "Error"
-    str_w_desc = "Error"
+    String is returned in format:
+        [n]) [Train Station] - [HH:MM Time]
 
-draw.text((10, 170), str_w_curr, inky_display.BLACK, font_l)
-draw.text((10, 220), str_w_today, inky_display.BLACK, font_s)
-draw.text((10, 250), str_w_desc, inky_display.BLACK, font_s)
-draw.text((10, 10), str_date_time, inky_display.BLACK, font_l)
-draw.text((10, 60), str_train1, inky_display.BLACK, font_s)
-draw.text((10, 90), str_train2, inky_display.BLACK, font_s)
-draw.text((10, 120), str_train3, inky_display.BLACK, font_s)
+    Args:
+        data (dict): dictionary data from json request
+        n (int): Train number
 
+    Returns:
+        str: Formatted string
+    """
+    try:
+        str_station = data['trainServices'][n - 1]['origin'][0]['locationName']
+        str_train_t = data['trainServices'][n - 1]['sta']
+        return f'{str(n)}) {str_station} - {str_train_t}'
+    except (KeyError, TypeError):
+        return "Error retrieving train data"
+
+
+def celsius_str_to_farenheit(c_temp: str) -> float:
+    """Convert C to F
+
+    Args:
+        c_temp (str): String temperature in Celsius
+
+    Returns:
+        float: Temperature in Farenheit
+    """
+    return "{:.1f}".format(float(c_temp) * 9 / 5 + 32)
+
+
+def gen_curr_weather(data: dict, in_celsius: bool = True) -> str:
+    """Generate current weather string
+
+    String is returned in format:
+        [XX.X]°[C/F] - [Current Weather]
+
+    Args:
+        data (dict): dictionary data from json request
+        in_celsius (bool): if True, formats for C; otherwise formats for F
+
+    Returns:
+        str: Formatted string
+    """
+    try:
+        temp_C = "{:.1f}".format(data['current']['temp'] + KELVIN_CONV_C)
+        str_temp = temp_C + DEG_C if in_celsius else \
+            celsius_str_to_farenheit(temp_C) + DEG_F
+        str_status = data['current']['weather'][0]['main']
+        return f'{str_temp} - {str_status}'
+    except (KeyError, TypeError):
+        return "Error retrieving weather"
+
+
+def gen_today_temp_range(data: dict, in_celsius: bool = True) -> str:
+    """Generate today's temperature range string
+
+    String is returned in format:
+        Today: [XX.X min]°[C/F]–[XX.X max]°[C/F]
+
+    Args:
+        data (dict): dictionary data from json request
+        in_celsius (bool): if True, formats for C; otherwise formats for F
+
+    Returns:
+        str: Formatted string
+    """
+    try:
+        temp_C_min = "{:.1f}".format(data_w['daily'][0]['temp']['min'] +
+                                     KELVIN_CONV_C)
+        temp_C_max = "{:.1f}".format(data_w['daily'][0]['temp']['max'] +
+                                     KELVIN_CONV_C)
+        str_temp_min = temp_C_min + DEG_C if in_celsius else \
+            celsius_str_to_farenheit(temp_C_min) + DEG_F
+        str_temp_max = temp_C_max + DEG_C if in_celsius else \
+            celsius_str_to_farenheit(temp_C_max) + DEG_F
+        return f'Today: {str_temp_min}–{str_temp_max}'
+    except (KeyError, TypeError):
+        return "Error retrieving temperature range"
+
+
+def gen_today_weather_condition(data: dict) -> str:
+    """Generate today's weather condition string
+
+    String is returned in format:
+        [weather condition]
+
+    Args:
+        data (dict): dictionary data from json request
+
+    Returns:
+        str: Formatted string
+    """
+    try:
+        return data['daily'][0]['weather'][0]['description']
+    except (KeyError, TypeError):
+        return "Error retrieving weather conditions"
+
+
+# Text retrieval and drawing
+SX = 10  # X offset
+SY = 10  # Y offset
+
+draw.text((SX, SY), strftime('%a %d %b %Y - %H:%M'), IBLACK, FONT_L)
+draw.text((SX, SY + 50), gen_train_string(data_t, 1), IBLACK, FONT_S)
+draw.text((SX, SY + 80), gen_train_string(data_t, 2), IBLACK, FONT_S)
+draw.text((SX, SY + 110), gen_train_string(data_t, 3), IBLACK, FONT_S)
+draw.text((SX, SY + 160), gen_curr_weather(data_w), IBLACK, FONT_L)
+draw.text((SX, SY + 210), gen_today_temp_range(data_w), IBLACK, FONT_S)
+draw.text((SX, SY + 240), gen_today_weather_condition(data_w), IBLACK, FONT_S)
+
+# Inky display image rendering
 inky_display.set_image(img)
 inky_display.show()
