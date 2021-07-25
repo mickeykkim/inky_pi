2900e2366ba1839a4ed7e9186515a449e43603fc
Mickey Kim <mickeykkim@gmail.com>
2021-07-25 19:50:25 +0100

    Add KeyError checking to openldbws.py

--- inky_pi/train/openldbws.py
+++ inky_pi/train/openldbws.py
@@ -66,14 +66,14 @@ class OpenLDBWS(TrainBase):
             dest_stn_abbr: str = abbreviate_stn_name(dest_stn)
             status: str = service.etd
             return f'{arrival_t} | P{platform} to {dest_stn_abbr} - {status}'
-        except (AttributeError, TypeError, IndexError):
+        except (AttributeError, TypeError, KeyError, IndexError):
             try:
                 # Try to get the error message & line wrap over each line
                 l_length: int = 41
                 return str(
                     self._data.nrccMessages[0].value[(num - 1) * l_length:num *
-                                                  l_length])
-            except (AttributeError, TypeError, IndexError):
+                                                     l_length])
+            except (AttributeError, TypeError, KeyError, IndexError):
                 # Check if any trains are running
                 if self._data.trainServices is None and num == 1:
                     dest: str = self._data.filterLocationName
