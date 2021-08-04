e090c0448a9c373dd1397fb69e852ed0cdd76868
Mickey Kim <mickeykkim@gmail.com>
2021-08-04 21:20:58 +0100

    Rename main.py variables for clarity

--- inky_pi/configs.py
+++ inky_pi/configs.py
@@ -5,40 +5,42 @@ from environs import Env
 _env: Env = Env()
 _env.read_env()
 
-T_MODEL: str = _env.str("T_MODEL", default='openldbws')
+TRAIN_MODEL: str = _env.str("TRAIN_MODEL", default='openldbws')
 """Train model to use"""
 
 # Train constants
-T_STATION_FROM: str = _env.str("T_STATION_FROM", default='MZH')
+STATION_FROM: str = _env.str("STATION_FROM", default='MZH')
 """From (departing) station"""
 
-T_STATION_TO: str = _env.str("T_STATION_TO", default='LBG')
+STATION_TO: str = _env.str("STATION_TO", default='LBG')
 """To (arrival) station"""
 
-T_NUM: int = _env.int("T_NUM", default=3)
+TRAIN_NUMBER: int = _env.int("TRAIN_NUMBER", default=3)
 """Number of departing trains to fetch"""
 
 # Token: https://realtime.nationalrail.co.uk/OpenLDBWSRegistration/Registration
-# Keep this in a .env file in project root folder i.e.: T_LDB_TOKEN=keyvalue
-T_LDB_TOKEN: str = _env.str("T_LDB_TOKEN", default='keep-in-.env-file')
+# Keep this in a .env file in project root dir i.e.: TRAIN_API_TOKEN=keyvalue
+TRAIN_API_TOKEN: str = _env.str("TRAIN_API_TOKEN", default='keep-in-.env-file')
 """OpenLDBWS API Token"""
 
-T_WSDL: str = _env.str("T_WDSL",
-                       default='http://lite.realtime.nationalrail.co.uk/' +
-                       'OpenLDBWS/wsdl.aspx?ver=2017-10-01')
+TRAIN_MODEL_URL: str = _env.str(
+    "TRAIN_MODEL_URL",
+    default='http://lite.realtime.nationalrail.co.uk/' +
+    'OpenLDBWS/wsdl.aspx?ver=2017-10-01')
 """OpenLDBWS API Endpoint"""
 
 # Weather constants; default London
-W_LATITUDE: float = _env.float("W_LATITUDE", default=51.5085)
+LATITUDE: float = _env.float("LATITUDE", default=51.5085)
 """Target Latitude"""
 
-W_LONGITUDE: float = _env.float("W_LONGITUDE", default=-0.1257)
+LONGITUDE: float = _env.float("LONGITUDE", default=-0.1257)
 """Target Longitude"""
 
-W_EXCLUDE: str = _env.str("W_EXCLUDE", default='minutely,hourly')
+EXCLUDE_FLAGS: str = _env.str("EXCLUDE_FLAGS", default='minutely,hourly')
 """Weather data exclude flags"""
 
 # OpenWeatherMap API Key: https://home.openweathermap.org/api_keys
-# Keep this in a .env file in project root folder i.e.: W_API_KEY=keyvalue
-W_API_KEY: str = _env.str("W_API_KEY", default='keep-in-.env-file')
+# Keep this in a .env file in project root dir i.e.: WEATHER_API_TOKEN=keyvalue
+WEATHER_API_TOKEN: str = _env.str("WEATHER_API_TOKEN",
+                                  default='keep-in-.env-file')
 """OpenWeatherMap API Key"""
--- inky_pi/main.py
+++ inky_pi/main.py
@@ -5,9 +5,9 @@ from typing import Dict
 
 from inky import InkyWHAT  # type: ignore
 
-from inky_pi.configs import (T_LDB_TOKEN, T_MODEL, T_NUM, T_STATION_FROM,
-                             T_STATION_TO, T_WSDL, W_API_KEY, W_EXCLUDE,
-                             W_LATITUDE, W_LONGITUDE)
+from inky_pi.configs import (EXCLUDE_FLAGS, LATITUDE, LONGITUDE, STATION_FROM,
+                             STATION_TO, TRAIN_API_TOKEN, TRAIN_MODEL,
+                             TRAIN_MODEL_URL, TRAIN_NUMBER, WEATHER_API_TOKEN)
 from inky_pi.display.inky_draw import InkyDraw  # type: ignore
 from inky_pi.train.huxley2_openldbws import HuxleyOpenLDBWS  # type: ignore
 from inky_pi.train.openldbws import OpenLDBWS  # type: ignore
@@ -17,41 +17,41 @@ from inky_pi.weather.weather_base import ScaleType, WeatherBase  # type: ignore
 
 
 # pylint: disable=unused-argument
-def _instantiate_huxley2(t_station_from: str, t_station_to: str, t_num: int,
+def _instantiate_huxley2(station_from: str, station_to: str, number: int,
                          *args) -> HuxleyOpenLDBWS:
-    return HuxleyOpenLDBWS(t_station_from, t_station_to, t_num)
+    """Huxley OpenLDBWS does not require a url or api key"""
+    return HuxleyOpenLDBWS(station_from, station_to, number)
 
 
-def _instantiate_openldbws(t_station_from: str, t_station_to: str, t_num: int,
-                           t_wsdl: str, t_ldb_token: str) -> OpenLDBWS:
-    return OpenLDBWS(t_station_from, t_station_to, t_num, t_wsdl, t_ldb_token)
+def _instantiate_openldbws(station_from: str, station_to: str, number: int,
+                           url: str, token: str) -> OpenLDBWS:
+    return OpenLDBWS(station_from, station_to, number, url, token)
 
 
-def _train_model_factory(t_model: str,
-                         t_station_from: str,
-                         t_station_to: str,
-                         t_num: int,
-                         t_wsdl: str = None,
-                         t_ldb_token: str = None) -> TrainBase:
+def _train_model_factory(model: str,
+                         station_from: str,
+                         station_to: str,
+                         number: int,
+                         url: str = None,
+                         token: str = None) -> TrainBase:
     """Selects and instantiates the defined train model to use
 
     Args:
-        t_model (str): Train model name, "huxley2" or "openldbws"
-        t_station_from (str): Departure station
-        t_station_to (str): Destination station
-        t_num (int): Number of departing trains to retrieve
-        t_wdls (str): [Optional] OpenLDBWS WSDL URL
-        t_ldb_token (str): [Optional] OpenLDBWS API token
+        model (str): Train model name, "huxley2" or "openldbws"
+        station_from (str): Departure station
+        station_to (str): Destination station
+        number (int): Number of departing trains to retrieve
+        url (str): [Optional] OpenLDBWS WSDL URL
+        token (str): [Optional] OpenLDBWS API token
     """
-    if t_model == "openldbws" and (t_wsdl is None or t_ldb_token is None):
+    if model == "openldbws" and (url is None or token is None):
         raise ValueError('OpenLDBWS requires WSDL and LDB token.')
 
     train_dict: Dict[str, TrainBase] = {
         "openldbws": _instantiate_openldbws,
         "huxley2": _instantiate_huxley2,
     }
-    return train_dict[t_model](t_station_from, t_station_to, t_num, t_wsdl,
-                               t_ldb_token)
+    return train_dict[model](station_from, station_to, number, url, token)
 
 
 def main() -> None:
@@ -61,12 +61,14 @@ def main() -> None:
     weather icon, and draws to inkyWHAT screen.
     """
     # Send requests to API endpoints to set data
-    train_data: TrainBase = _train_model_factory(T_MODEL, T_STATION_FROM,
-                                                 T_STATION_TO, T_NUM, T_WSDL,
-                                                 T_LDB_TOKEN)
+    train_data: TrainBase = _train_model_factory(TRAIN_MODEL, STATION_FROM,
+                                                 STATION_TO, TRAIN_NUMBER,
+                                                 TRAIN_MODEL_URL,
+                                                 TRAIN_API_TOKEN)
 
-    weather_data: WeatherBase = OpenWeatherMap(W_LATITUDE, W_LONGITUDE,
-                                               W_EXCLUDE, W_API_KEY)
+    weather_data: WeatherBase = OpenWeatherMap(LATITUDE, LONGITUDE,
+                                               EXCLUDE_FLAGS,
+                                               WEATHER_API_TOKEN)
 
     # Set the display object configured with specified Inky display model
     display = InkyDraw(InkyWHAT('black'))
@@ -74,7 +76,7 @@ def main() -> None:
     # Draw text and weather icon on display object at specified x, y coords
     display.draw_date(10, 10)
     display.draw_time(297, 10)
-    display.draw_train_times(train_data, T_NUM, 10, 60)
+    display.draw_train_times(train_data, TRAIN_NUMBER, 10, 60)
     display.draw_weather_forecast(weather_data, ScaleType.celsius, 10, 160)
     display.draw_weather_icon(weather_data.get_icon(), 300, 200)
 
