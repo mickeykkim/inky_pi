faed2d6c3282d4eee817b929915528834be00639
Mickey Kim <mickeykkim@gmail.com>
2022-01-10 00:38:21 +0000

    Update click cli w/ separate methods

--- inky_pi/cli.py
+++ inky_pi/cli.py
@@ -1,14 +1,31 @@
 """Console script for inky_pi."""
 import click
 
-import inky_pi.main as inky_main
+import inky_pi.goodnight
+import inky_pi.main
 
 
-@click.command()
+@click.group()
+def cli():
+    """CLI group for inky_pi."""
+    ...
+
+
+@cli.command()
+def inky():
+    """Console script for inky_pi main."""
+    inky_pi.main.main()
+
+
+@cli.command()
+def goodnight():
+    """Console script for inky_pi goodnight."""
+    inky_pi.goodnight.main()
+
+
 def main():
-    """Console script for inky_pi."""
-    inky_main.main()
-    return 0
+    """CLI main method."""
+    cli()
 
 
 if __name__ == "__main__":
